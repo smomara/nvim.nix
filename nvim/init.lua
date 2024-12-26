e522a29a41443fa8b46e3e19fc81e6341db23517
Sean O'Mara <omarams@cua.edu>
2024-12-26 10:58:06 -0500

    gruvbox

--- nvim/init.lua
+++ nvim/init.lua
@@ -101,3 +101,7 @@ cmd.packadd('cfilter') -- Allows filtering the quickfix list with :cfdo
 
 -- let sqlite.lua (which some plugins depend on) know where to find sqlite
 vim.g.sqlite_clib_path = require('luv').os_getenv('LIBSQLITE')
+
+-- set colorscheme
+vim.o.background = "dark"
+vim.cmd('colorscheme gruvbox')
