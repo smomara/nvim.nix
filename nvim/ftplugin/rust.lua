9bcb7bc6931a28cb8961d2bac4558f6ee6f42ae1
Sean O'Mara <omarams@cua.edu>
2025-01-15 17:45:57 -0500

    rust refresh support

--- nvim/ftplugin/rust.lua
+++ nvim/ftplugin/rust.lua
@@ -5,16 +5,10 @@ if vim.fn.executable(rust_analyzer_cmd) ~= 1 then
   return
 end
 
-local root_files = {
-  'Cargo.toml',
-  'rust-project.json',
-  '.git',
-}
-
 vim.lsp.start({
   name = 'rust-analyzer',
   cmd = { rust_analyzer_cmd },
-  root_dir = vim.fs.dirname(vim.fs.find(root_files, { upward = true })[1]),
+  root_dir = vim.fs.dirname(vim.fs.find('Cargo.toml', { upward = true })[1]),
   capabilities = require('user.lsp').make_client_capabilities(),
   settings = {
     ['rust-analyzer'] = {
@@ -29,6 +23,7 @@ vim.lsp.start({
         experimental = {
           enable = true,
         },
+        refreshSupport = false,
       },
       inlayHints = {
         enable = true,
