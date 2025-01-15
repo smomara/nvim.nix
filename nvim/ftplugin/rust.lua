45600f1cc972edcdf50f411727c820eb56e404e3
Sean O'Mara <omarams@cua.edu>
2025-01-15 16:24:17 -0500

    rust-analyzer

---
+++ nvim/ftplugin/rust.lua
@@ -0,0 +1,38 @@
+local rust_analyzer_cmd = 'rust_analyzer'
+
+if vim.fn.executable(rust_analyzer_cmd) ~= 1 then
+  vim.notify('rust-analyzer not found. Please install it.', vim.log.levels.WARN)
+  return
+end
+
+local root_files = {
+  'Cargo.toml',
+  'rust-project.json',
+  '.git',
+}
+
+vim.lsp.start({
+  name = 'rust-analyzer',
+  cmd = { rust_analyzer_cmd },
+  root_dir = vim.fs.dirname(vim.fs.find(root_files, { upward = true })[1]),
+  capabilities = require('user.lsp').make_client_capabilities(),
+  settings = {
+    ['rust-analyzer'] = {
+      checkOnSave = {
+        command = 'clippy',
+      },
+      cargo = {
+        allFeatures = true,
+      },
+      diagnostics = {
+        enable = true,
+        experimental = {
+          enable = true,
+        },
+      },
+      inlayHints = {
+        enable = true,
+      },
+    },
+  },
+})
